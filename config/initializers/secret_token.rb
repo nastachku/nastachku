ad547de9d9a279a41c3fd7e21516f7728fa2f64a
Mikhail Stolbov <mstolbov@gmail.com>
2013-01-14 19:41:57 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nastachku::Application.config.secret_token = '7b447549541e50b4950115830d44d08aa1cc66ed1203fc813d115a85d263674c6d77a768b2d108be56908469b11ea5b8e61698c23d471ed5e2210005f0768370'
