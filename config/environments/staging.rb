d11fe8c07bb18fc722978eaf45927bc4d71bb2e1
Heimdell <hindmost.one@gmail.com>
2013-01-23 17:01:12 +0400

    поправил имя проекта в конфиге окружения стейджинга

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Castle::Application.configure do
+Nastachku::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
