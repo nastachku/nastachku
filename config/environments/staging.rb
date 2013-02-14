31fdab760fc3697dfb2d9d8b79363ee856947b5b
PanfilovDenis <panf.denis@gmail.com>
2013-02-14 17:44:08 +0000

    precompile fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ Nastachku::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,9 @@ Nastachku::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( ckeditor/* )
+  # config.assets.precompile += %w( ckeditor/* )
+  config.assets.precompile += %w( admin/application.js admin/application.css )
+  
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
