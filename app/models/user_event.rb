ac48f06c4a67370c51f1cd7466b86187759403a6
Alexey Frank <alexeyfrank@gmail.com>
2013-02-18 17:58:34 +0400

    remove validates from user_event presentation field

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -3,7 +3,7 @@ class UserEvent < BaseEvent
 
   validates :title, presence: true
   validates :thesises, presence: true
-  validates :presentation, presence: true
+  #validates :presentation, presence: true
 
   belongs_to :speaker, class_name: 'User'
   has_many :votings, foreign_key: 'event_id'
