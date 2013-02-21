1351ea12098eeb5f13771fda42f8eedd4b5b8739
PanfilovDenis <panf.denis@gmail.com>
2013-02-21 12:57:47 +0000

    fix regular in russian validation

--- lib/russian_validator.rb
+++ lib/russian_validator.rb
@@ -2,7 +2,7 @@
 
 class RussianValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A[а-я -]+\Z/iu
+    unless value =~ /\A[а-я -]*\Z/iu
       record.errors.add(attribute, (options[:message] || :wrong_language))
     end
   end
