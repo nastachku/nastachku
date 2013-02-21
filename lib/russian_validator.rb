c28489ad1c8fa4458804d781c00cbc9b40435d64
PanfilovDenis <panf.denis@gmail.com>
2013-02-21 12:09:56 +0000

    rafactoring russian validator

--- lib/russian_validator.rb
+++ lib/russian_validator.rb
@@ -3,7 +3,7 @@
 class RussianValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A[а-я -]+\Z/iu
-      record.errors[attribute] << (options[:message] || "Поле должно содержать только русские буквы")
+      record.errors.add(attribute, (options[:message] || :wrong_language))
     end
   end
 end
