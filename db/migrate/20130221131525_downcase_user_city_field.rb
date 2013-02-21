cfc20226019735ee45d5fe554a6bb4e7727f2e2e
PanfilovDenis <panf.denis@gmail.com>
2013-02-21 15:38:27 +0000

    fix downcase user city field

--- db/migrate/20130221131525_downcase_user_city_field.rb
+++ db/migrate/20130221131525_downcase_user_city_field.rb
@@ -1,8 +1,9 @@
 class DowncaseUserCityField < ActiveRecord::Migration
   def up
     User.find_each do |user|
-      if user.city
-        user.update_attributes city: user.city.downcase
+      if user.city?
+        user.city = user.city.mb_chars.downcase
+        user.save validate: false
       end
     end
   end
