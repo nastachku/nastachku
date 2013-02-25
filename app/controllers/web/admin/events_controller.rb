81e17204c2fd00cefa24e02a735c725d0df49199
Alexey Frank <alexeyfrank@gmail.com>
2013-02-25 17:43:54 +0400

    fix admin/events controller

--- app/controllers/web/admin/events_controller.rb
+++ app/controllers/web/admin/events_controller.rb
@@ -1,7 +1,7 @@
 class Web::Admin::EventsController < Web::Admin::ApplicationController
 
   def index
-  	@events = BaseEvent.asc_by_start_time.asc_by_workshop_id
+  	@events = BaseEvent.by_start_time.asc_by_workshop_id
   end
 
   def new 
