8147bf9d73359d3010ea930e7b58e47d2de93c80
Alexey Frank <alexeyfrank@gmail.com>
2013-02-25 12:09:27 +0400

    add title helper

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -4,5 +4,18 @@ class Web::ApplicationController < ApplicationController
   include Web::LectorsHelper
 
   protect_from_forgery
-  
+
+  helper_method :title
+
+  private
+
+  def title(part = nil)
+    @parts ||= []
+    unless part
+      return nil if @parts.blank?
+      return @parts.reverse.join(' - ')
+    end
+    @parts << part
+  end
+
 end
