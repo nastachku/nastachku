b8d4bf4f3b5baa83de855092e652ec40a5dab80b
PanfilovDenis <panf.denis@gmail.com>
2013-02-26 12:58:34 +0000

    refactoring capistrano deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,12 +33,12 @@ namespace :deploy do
   namespace :assets do
 
     task :precompile, roles: :app do
-      run_locally "rake assets:clean && rake assets:precompile"
+      run_locally "RAILS_ENV=#{rails_env} #{rake} assets:clean && RAILS_ENV=#{rails_env} #{rake} assets:precompile"
       run_locally "cd public && tar -jcf assets.tar.bz2 assets"
       top.upload "public/assets.tar.bz2", "#{shared_path}", via: :scp
       run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
       run_locally "rm public/assets.tar.bz2"
-      run_locally "rake assets:clean"
+      run_locally "#{rake} assets:clean"
     end
 
     task :symlink, roles: :app do
