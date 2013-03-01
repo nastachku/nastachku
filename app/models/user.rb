01e39c821fd644a071b0e81d09afaca659f06ffe
PanfilovDenis <panf.denis@gmail.com>
2013-03-01 11:24:03 +0000

    add carousel_info field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
                   :first_name, :last_name, :city,
                   :company, :position,
                   :show_as_participant,
-                  :photo, :state_event, :about
+                  :photo, :state_event, :about, :carousel_info
 
   audit :email, :first_name, :last_name, :city, :company, :photo, :state, :about
 
