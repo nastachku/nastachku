62dc7571c9a60a06b848bcd8272e1002ca879a25
Alexey Frank <alexeyfrank@gmail.com>
2013-03-04 17:31:27 +0400

    fix scope for news

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class Web::WelcomeController < Web::ApplicationController
   def index
-    @news = News.last3
+    @news = News.last(3)
     @lectors = User.as_lectors.by_created_at
   end
 
--- app/repositories/news_repository.rb
+++ app/repositories/news_repository.rb
@@ -6,6 +6,6 @@ module NewsRepository
 
   included do
     scope :web, ->{by_created_at}
-    scope :last3, -> { by_created_at.limit(3) }
+    scope :last, lambda { |count| by_created_at.limit(count) }
   end
 end
