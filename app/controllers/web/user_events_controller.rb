db727e14f5c6e8b769b28c76f227fc2850f8f603
Alexey Frank <alexeyfrank@gmail.com>
2013-03-05 13:40:47 +0400

    hide events link in menu

--- app/controllers/web/user_events_controller.rb
+++ app/controllers/web/user_events_controller.rb
@@ -2,7 +2,7 @@ class Web::UserEventsController < Web::ApplicationController
 
   def index
     @search = UserEvent.ransack(params[:q])
-    @events = @search.result.voted.with_active_speaker.asc_by_created_at
+    @events = @search.result.voted.with_active_speaker.by_created_at
   end
 end
 
