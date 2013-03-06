e2397d32744400d973c5ee3b40e71d5f24233c02
Alexey Frank <alexeyfrank@gmail.com>
2013-03-06 13:24:10 +0400

    fix sorting in user events

--- app/controllers/web/user_events_controller.rb
+++ app/controllers/web/user_events_controller.rb
@@ -1,7 +1,7 @@
 class Web::UserEventsController < Web::ApplicationController
 
   def index
-    conditions = params[:q]  || { s: %w[ created_at asc ] }
+    conditions = params[:q]  || { s: %w[ created_at desc ] }
     @search = UserEvent.ransack conditions
     @events = @search.result.voted.with_active_speaker.by_created_at
   end
