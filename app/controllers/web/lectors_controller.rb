16902afd0481ab484437f957a76eb8368263ccf3
Alexey Frank <alexeyfrank@gmail.com>
2013-03-06 16:17:28 +0400

    add titles for lectors controller

--- app/controllers/web/lectors_controller.rb
+++ app/controllers/web/lectors_controller.rb
@@ -2,7 +2,7 @@ class Web::LectorsController < Web::ApplicationController
   def index
     @lectors = User.activated.as_lectors.by_created_at.ransack(params).result
     if params[:q]
-      @topic = Topic.find params[:q][:topics_id_eq]
+      @topic = Topic.find params[:topics_id_eq]
       title @topic
     end
   end
