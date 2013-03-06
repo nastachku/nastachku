95f55baee716d7f4ce160ee924ce607b4055be6e
Alexey Frank <alexeyfrank@gmail.com>
2013-03-06 11:18:31 +0400

    change navigation menu

--- app/controllers/web/events_controller.rb
+++ app/controllers/web/events_controller.rb
@@ -1,9 +1,8 @@
 class Web::EventsController < Web::ApplicationController
   def index
-    @events = UserEvent.scheduled.with_active_speaker.by_listener_votes
-    if params[:q]
-      @events = @events.ransack(params[:q]).result
-      @workshop = Workshop.find params[:q][:workshop_id_eq]
+    @events = UserEvent.scheduled.with_active_speaker.by_listener_votes.ransack(params).result
+    if params[:workshop_id_eq]
+      @workshop = Workshop.find params[:workshop_id_eq]
       title @workshop
     end
   end
