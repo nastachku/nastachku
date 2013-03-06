cdc3b03880f4b71c9cc7d2c18019e5b2a2913dad
PanfilovDenis <panf.denis@gmail.com>
2013-03-06 11:58:10 +0000

    fix carousel

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -2,7 +2,7 @@
 class Web::WelcomeController < Web::ApplicationController
   def index
     @news = News.last(3)
-    @lectors = User.as_lectors.in_carousel.by_created_at
+    @lectors = User.as_lectors.in_carousel.activated.by_created_at
   end
 
 end
