815aa351d4732c9e550d92c78fb72760e313bd7e
PanfilovDenis <panf.denis@gmail.com>
2013-03-07 09:19:49 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Nastachku::Application.routes.draw do
   end
 
   scope :module => :web do
-    resource :user do
+    resource :user, only: [] do
       get :activate
     end
     resources :users, only: [:new, :create, :index]
