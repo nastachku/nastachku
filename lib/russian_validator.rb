cf6b75e6d218213d15d08e6f504271d0fda6a121
PanfilovDenis <panf.denis@gmail.com>
2013-03-12 10:30:43 +0000

    fix russian validator #179

--- lib/russian_validator.rb
+++ lib/russian_validator.rb
@@ -2,7 +2,7 @@
 
 class RussianValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if !(value.blank? || (value =~ /\A[а-я -]*\Z/iu))
+    if !(value.blank? || (value =~ /\A[а-яё -]*\Z/iu))
       record.errors.add(attribute, (options[:message] || :wrong_language))
     end
   end
