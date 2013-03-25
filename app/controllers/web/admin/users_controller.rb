d5af1180b80dc05cd0e53280aa9e9e09de653013
PanfilovDenis <panf.denis@gmail.com>
2013-03-25 19:44:37 +0000

    add default ransack sorting

--- app/controllers/web/admin/users_controller.rb
+++ app/controllers/web/admin/users_controller.rb
@@ -18,12 +18,9 @@ class Web::Admin::UsersController < Web::Admin::ApplicationController
   end
 
   def index
-    @search = User.ransack(params[:q])
-    if params[:q]
-      @users = @search.result.page(params[:page]).per(50)
-    else
-      @users = @search.result.alphabetically.page(params[:page]).per(50)
-    end
+    query = { s: 'created_at desc' }.merge(params[:q] || {})
+    @search = User.ransack(query)
+    @users = @search.result.page(params[:page]).per(50)
   end
 
   def show
