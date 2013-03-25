1079b1710427bf92190301a3f8d8ccf7ca352715
PanfilovDenis <panf.denis@gmail.com>
2013-03-25 08:58:42 +0000

    add distinct to lectors

--- app/controllers/web/lectors_controller.rb
+++ app/controllers/web/lectors_controller.rb
@@ -1,6 +1,6 @@
 class Web::LectorsController < Web::ApplicationController
   def index
-    @lectors = User.activated.as_lectors.by_created_at.ransack(params).result
+    @lectors = User.activated.as_lectors.by_created_at.ransack(params).result(distinct: true)
     @workshops = Workshop.web
   end
 end
