1f83649cf57b19560b68cbdfe4c56ae729d14f54
PanfilovDenis <panf.denis@gmail.com>
2013-03-25 12:41:29 +0000

    add ransack to admin/audits

--- app/controllers/web/admin/audits_controller.rb
+++ app/controllers/web/admin/audits_controller.rb
@@ -1,5 +1,12 @@
 class Web::Admin::AuditsController < Web::Admin::ApplicationController
   def index
-    @audits = Auditable::Audit.order('created_at DESC').page(params[:page]).per(configus.pagination.audits_per_page)
+
+    @search = Auditable::Audit.ransack(params[:q])
+    if params[:q]
+      @audits = @search.result.page(params[:page]).per(configus.pagination.admin_per_page)
+    else
+      @audits = @search.result.order('created_at DESC').page(params[:page]).per(configus.pagination.admin_per_page)
+    end
+
   end
 end
