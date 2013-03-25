dec6d20e91d3b2ddf9757a1fe16051de68288fcb
PanfilovDenis <panf.denis@gmail.com>
2013-03-25 11:53:26 +0000

    add ransack to admin/orders

--- app/controllers/web/admin/orders_controller.rb
+++ app/controllers/web/admin/orders_controller.rb
@@ -1,7 +1,12 @@
 class Web::Admin::OrdersController < Web::Admin::ApplicationController
 
   def index
-    @orders = Order.web
+    @search = Order.ransack(params[:q])
+    if params[:q]
+      @orders = @search.result.page(params[:page]).per(configus.pagination.admin_per_page)
+    else
+      @orders = @search.result.web.page(params[:page]).per(configus.pagination.admin_per_page)
+    end
   end
 
 end
