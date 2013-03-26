9f4dc06bdd8a33025309c306cfa7fd9ef09399bb
PanfilovDenis <panf.denis@gmail.com>
2013-03-26 09:39:03 +0000

    add affix

--- app/controllers/web/user_lectures_controller.rb
+++ app/controllers/web/user_lectures_controller.rb
@@ -1,10 +1,8 @@
 class Web::UserLecturesController < Web::ApplicationController
 
   def index
-    conditions = params || { s: "created_at desc" }
-    @search = Lecture.ransack conditions
-    @lectures = @search.result.voted.with_active_speaker.by_created_at
-
+    @lectures = Lecture.voted.with_active_speaker.by_created_at.ransack(params).result
+    @workshops = Workshop.web
   end
 
 end
