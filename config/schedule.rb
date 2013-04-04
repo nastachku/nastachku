ca3ce1fc251f99eb5bc337b82c8b1fcd9beb4949
PanfilovDenis <panf.denis@gmail.com>
2013-04-04 13:39:50 +0000

    change sync period

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,6 +27,6 @@ every 4.hours do
   bundle_command "backup perform -t nastachku_#{environment} -c config/backup.rb --root_path ."
 end
 
-every 4.hours do
+every 1.hours do
   rake "app:timepad_user_synchronization"
 end
