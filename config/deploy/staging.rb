ac476db3a1265eb21ca2dae6f8bc445196f6d0ad
PanfilovDenis <panf.denis@gmail.com>
2013-04-05 10:04:22 +0000

    add require whenever for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,3 +6,6 @@ set :keep_releases, 5
 role :web, '85.143.166.207'
 role :app, '85.143.166.207'
 role :db,  '85.143.166.207', :primary => true
+
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
