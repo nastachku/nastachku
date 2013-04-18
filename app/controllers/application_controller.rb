f5511e5d5bf4484bbe046fb2b0cbd1cd19aac2ef
PanfilovDenis <panf.denis@gmail.com>
2013-04-18 15:09:00 +0000

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include AuthHelper
   include Mobylette::RespondToMobileRequests
 
+  helper_method :current_user, :signed_in?, :signed_as_admin?
+
   before_filter :deny_banned_user!
 
 end
