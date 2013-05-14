09193702b3a65b0340aa39645ecc52f66f056816
Alexey Frank <alexeyfrank@gmail.com>
2013-05-14 21:01:15 +0400

    change production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,9 +8,9 @@ end
 set :user, 'nastachku_production'
 set :keep_releases, 5
 
-role :web, '62.76.184.142'
-role :app, '62.76.184.142'
-role :db,  '62.76.184.142', :primary => true
+role :web, '62.76.191.134'
+role :app, '62.76.191.134'
+role :db,  '62.76.191.134', :primary => true
 
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
