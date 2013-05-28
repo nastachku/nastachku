84e8171971b0c5c58b4037c6df7362784300894f
Alex Klyanchin <jetblackmeerkat@gmail.com>
2013-05-28 20:13:02 +0400

    add universal state collection input

--- app/inputs/state_event_input.rb
+++ app/inputs/state_event_input.rb
@@ -1,6 +1,8 @@
 class StateEventInput < SimpleForm::Inputs::CollectionSelectInput
   def collection
-    object.send("state_transitions")
+    clean_attribute_name = attribute_name.to_s.gsub '_event', ''
+    transitions = "#{clean_attribute_name}_transitions"
+    object.send(transitions).map { |t| [t.human_event, t.event] }
   end
 
   def input
