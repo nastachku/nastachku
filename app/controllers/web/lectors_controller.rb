9adb4a774e6eefb7bad369647856bdf63ef08d1c
Dmitry Davydov <haudvd@gmail.com>
2014-02-03 20:57:03 +0400

    fix

--- app/controllers/web/lectors_controller.rb
+++ app/controllers/web/lectors_controller.rb
@@ -1,6 +1,6 @@
 class Web::LectorsController < Web::ApplicationController
   def index
-    @lectors = User.activated.as_lectors.visible.by_created_at.ransack(params).result(distinct: true)
+    @lectors = UserDecorator.decorate(User.activated.as_lectors.visible.by_created_at.ransack(params).result(distinct: true))
     @workshops = Workshop.web
   end
 end
