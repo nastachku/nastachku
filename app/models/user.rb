18e945eee71d4055e8a70450c11e0e08fda2ece1
Dmitry Davydov <haudvd@gmail.com>
2014-02-12 03:03:51 +0400

    fix user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'digest/md5'
 
 class User < ActiveRecord::Base
@@ -11,11 +12,11 @@ class User < ActiveRecord::Base
   audit :email, :first_name, :last_name, :city, :company, :photo, :state, :about
 
   validates :email, presence: true, uniqueness: {case_sensitive: false}, email: true
-  validates :first_name, length: { maximum: 255 }, russian: true
-  validates :last_name, length: { maximum: 255 }, russian: true
-  validates :city, length: { maximum: 255 }, russian: true
-  validates :company, length: { maximum: 255 }
-  validates :position, length: { maximum: 255 }
+  validates_format_of :first_name, with: /^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я '`-]{0,253}[a-zA-Zа-яА-Я]$/
+  validates_format_of :last_name, with: /^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я '`-]{0,253}[a-zA-Zа-яА-Я]$/
+  validates_format_of :city, with: /^[a-zA-Zа-яА-Я0-9 '`-]{0,255}$/
+  validates_format_of :company, with: /^[a-zA-Zа-яА-Я0-9 '`~!@#$%^&*()_+=\-"{}\[\]\/\\|;:,.?<>-]{0,255}$/
+  validates_format_of :position, with: /^[a-zA-Zа-яА-Я0-9 '`-]{0,255}$/
 
   enumerize :role, in: [ :lector, :user ], default: :user
   has_many :lectures, dependent: :destroy
