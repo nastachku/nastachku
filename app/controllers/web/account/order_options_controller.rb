189bd343d010d7df1bce84d38a68ed9778b1f16a
pavel <kalashnikovisme@gmail.com>
2014-02-11 07:01:56 +0400

    pay is working

--- app/controllers/web/account/order_options_controller.rb
+++ app/controllers/web/account/order_options_controller.rb
@@ -4,8 +4,10 @@ class Web::Account::OrderOptionsController < Web::Account::ApplicationController
   def create
     @order_option = OrderOption.new params[:order_option]
     if @order_option.save
+      flash_success
       redirect_to edit_account_path
     else
+      flash_error
       redirect_to edit_account_path
     end
   end
--- app/models/ticket_order.rb
+++ app/models/ticket_order.rb
@@ -9,6 +9,12 @@ class TicketOrder < Order
   validates :ticket_type, presence: true
 
   def cost
-    self.items_count * configus.platidoma.ticket_price
+    if self.ticket_type.first?
+      configus.platidoma.ticket_price_first_day
+    elsif self.ticket_type.second?
+      configus.platidoma.ticket_price_second_day
+    elsif self.ticket_type.full?
+      configus.platidoma.ticket_price_first_day + configus.platidoma.ticket_price_second_day
+    end
   end
 end
