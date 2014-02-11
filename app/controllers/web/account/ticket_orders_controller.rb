f011e9602b7415fb4f56805f2d156a79916b2e8e
pavel <kalashnikovisme@gmail.com>
2014-02-11 05:40:55 +0400

    remove ticket_order/new action

--- app/controllers/web/account/ticket_orders_controller.rb
+++ app/controllers/web/account/ticket_orders_controller.rb
@@ -1,10 +1,6 @@
 class Web::Account::TicketOrdersController < Web::Account::ApplicationController
   skip_before_filter :authenticate_user!, only: [:new]
 
-  def new
-    @ticket_order = TicketOrder.new
-  end
-
   def create
     @ticket_order = current_user.ticket_orders.build params[:ticket_order]
     gon.price = configus.platidoma.ticket_price
--- test/functional/web/account/ticket_orders_controller_test.rb
+++ test/functional/web/account/ticket_orders_controller_test.rb
@@ -4,20 +4,15 @@ class Web::Account::TicketOrdersControllerTest < ActionController::TestCase
   setup do
     @user = create :user
     sign_in @user
-    @attrs = attributes_for :ticket_order
     @order = create :ticket_order
   end
 
-  test "should get new" do
-    get :new
-    assert_redirected_to root_path
-  end
-
   test "should post create" do
-    post :create, ticket_order: @attrs
+    attributes = attributes_for :ticket_order
+    post :create, ticket_order: attributes
 
     assert_response :redirect
-    order = current_user.orders.find_by_items_count @attrs[:items_count]
+    order = current_user.orders.find_by_items_count attributes[:items_count]
     assert order
   end
 end
