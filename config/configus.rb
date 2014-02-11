b4337531aa88bb6d1827dd0237b75215ff4106af
pavel <kalashnikovisme@gmail.com>
2014-02-11 08:28:50 +0400

    add edited of cost

--- config/configus.rb
+++ config/configus.rb
@@ -113,6 +113,16 @@ Configus.build Rails.env do
       gate_password "test"
       afterparty_price 1500
       shirt_price 500
+      if Time.current.month == 2
+        ticket_price_first_day 500
+        ticket_price_second_day 250
+      elsif Time.current.month == 3
+        ticket_price_first_day 750
+        ticket_price_second_day 350
+      elsif Time.current.month == 4
+        ticket_price_first_day 1000
+        ticket_price_second_day 500
+      end
     end
   end
 
