a47b1def2cbfd8dc688fd06df5ef647a97727d2c
pavel <kalashnikovisme@gmail.com>
2014-02-11 15:24:58 +0400

    add helper function

--- app/helpers/web/accounts_helper.rb
+++ app/helpers/web/accounts_helper.rb
@@ -10,4 +10,18 @@ module Web::AccountsHelper
   def item_color_collection
     ShirtOrder.item_color.values
   end
+
+  def user_has_bought_full_ticket?(user)
+    user.ticket_orders.any? { |t| t.ticket_type == :full }
+  end
+
+  def user_has_bought_first_and_second_days_tickets?(user)
+    if user.ticket_orders.any? { |t| t.ticket_type == :first_day}
+      user.ticket_orders.any? { |t| t.ticket_type == :second_day}
+    end
+  end
+
+  def user_has_full_ticket?(user)
+    user_has_bought_full_ticket?(user) or user_has_bought_first_and_second_days_tickets?(user)
+  end
 end
