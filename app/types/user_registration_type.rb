3b5aae6c664fc4c79afad3b5b9e4df5b548767da
pavel <kalashnikovisme@gmail.com>
2014-02-12 15:10:26 +0400

    regexp commented

--- app/types/user_registration_type.rb
+++ app/types/user_registration_type.rb
@@ -8,13 +8,13 @@ class UserRegistrationType < User
   validates :first_name,  presence: true
   validates :last_name,  presence: true
   validates :city, presence: true
-  validates_each :password do |record, attr, value|
-    unless value =~ /^.{8,30}$/ and value =~ /^.*[a-z]+.*$/ and
-        value =~ /^.*[A-Z]+.*$/ and value =~ /^.*[0-9]+.*$/ and
-        value =~ /^.*[`~!@#$%^&*()_+=\-{}\[\]\/\\|;:,.?<>-]+.*$/
-      record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
-    end
-  end
+  #validates_each :password do |record, attr, value|
+  #  unless value =~ /^.{8,30}$/ and value =~ /^.*[a-z]+.*$/ and
+  #      value =~ /^.*[A-Z]+.*$/ and value =~ /^.*[0-9]+.*$/ and
+  #      value =~ /^.*[`~!@#$%^&*()_+=\-{}\[\]\/\\|;:,.?<>-]+.*$/
+  #    record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
+  #  end
+  #end
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
   validates :process_personal_data, acceptance: true
