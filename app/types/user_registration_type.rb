acc9d9452a90dcfa2720e1f0b996e80cb452b3e9
Dmitry Davydov <haudvd@gmail.com>
2014-02-14 00:46:14 +0400

    fix regex

--- app/types/user_registration_type.rb
+++ app/types/user_registration_type.rb
@@ -11,7 +11,7 @@ class UserRegistrationType < User
   validates_each :password do |record, attr, value|
     unless value =~ /^.{8,30}$/ and value =~ /^.*[a-z]+.*$/ and
         value =~ /^.*[A-Z]+.*$/ and value =~ /^.*[0-9]+.*$/ and
-        value =~ /^.*[`~\!\@\#\$\%^&*\(\)_+=\-{}\[\]\/\\|;:,.?<>]+.*$/
+        value =~ /^.*[`~\!\@\#\$\%^&*\(\)_+=\-"{}\[\]\/\\|;:,.?<>]+.*$/
       record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
     end
   end
