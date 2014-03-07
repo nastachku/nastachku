dc86437df9125cf00e3322d138fdead13334856d
Dmitry Davydov <haudvd@gmail.com>
2014-03-07 15:06:38 +0400

    fix user change password functional

--- app/types/account_edit_type.rb
+++ app/types/account_edit_type.rb
@@ -8,8 +8,18 @@ class AccountEditType < User
   validates :last_name,  presence: true
   validates :city, presence: true
   validates_each :old_password do |record, attr, value|
-    if !value.empty? and !User.find(record.id).authenticate(value)
+    @old_password_is_empty = value.empty?
+    if !@old_password_is_empty and !User.find(record.id).authenticate(value)
       record.errors.add(attr, I18n.t(:password_is_wrong, scope: [:activerecord, :errors, :messages]))
+    end
+  end
+  validates_each :password do |record, attr, value|
+    unless @old_password_is_empty and (value.nil? or value.empty?)
+      unless value =~ /^.{8,30}$/ and value =~ /^.*[a-z]+.*$/ and
+          value =~ /^.*[A-Z]+.*$/ and value =~ /^.*[0-9]+.*$/ and
+          value =~ /^.*[`~\!\@\#\$\%^&*\(\)_+=\-"{}\[\]\/\\|;:,.?<>]+.*$/
+        record.errors.add(attr, I18n.t(:password_not_valid, scope: [:activerecord, :errors, :messages]))
+      end
     end
   end
 
