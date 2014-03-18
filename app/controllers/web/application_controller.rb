7f3290dfbabcf871685718460d6302fd349f5a87
Dmitry Davydov <haudvd@gmail.com>
2014-03-18 16:37:26 +0400

    fix controllers auth

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -22,6 +22,15 @@ class Web::ApplicationController < ApplicationController
   before_filter do
     title t('base_name')
     title t("#{params[:controller].gsub('/', '.')}.#{params[:action]}.title")
+
+    if params[:auth_token]
+      token = User::AuthToken.find_by_authentication_token params[:auth_token]
+      if token
+        user = token.user
+        @auth_email = user.email
+      end
+    end
+    @auth_user = UserSignInType.new
   end
 
   private
