56e00e031c422d6482df7c615cd35008fd88b3fd
pavel <kalashnikovisme@gmail.com>
2014-03-19 17:50:55 +0400

    output sections

--- app/controllers/web/schedules_controller.rb
+++ app/controllers/web/schedules_controller.rb
@@ -2,5 +2,6 @@ class Web::SchedulesController < Web::ApplicationController
   def show
     @halls = HallDecorator.decorate_collection Hall.activated.by_position
     @slots = Slot.all
+    @workshops = Workshop.all
   end
 end
