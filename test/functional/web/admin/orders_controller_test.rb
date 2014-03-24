6b2392c83b3a7fa585711e10f0baa7eb17fb10f0
pavel <kalashnikovisme@gmail.com>
2014-03-24 19:11:20 +0400

    add edit order functional

--- test/functional/web/admin/orders_controller_test.rb
+++ test/functional/web/admin/orders_controller_test.rb
@@ -4,6 +4,7 @@ class Web::Admin::OrdersControllerTest < ActionController::TestCase
 
   setup do
     user = create :admin
+    @order = create :order
     sign_in user
   end
 
@@ -12,4 +13,14 @@ class Web::Admin::OrdersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get edit" do
+    get :edit, id: @order
+    assert_response :success
+  end
+
+  test "should put update" do
+    attributes = attributes_for :order
+    put :update, id: @order, order: attributes
+    assert_response :redirect
+  end
 end
