601cab84bd10baa8dfa44eeb2ab5081cbe057ad7
pavel <kalashnikovisme@gmail.com>
2014-03-24 18:46:28 +0400

    add condition to platidoma approve

---
+++ app/decorators/order_decorator.rb
@@ -0,0 +1,8 @@
+class OrderDecorator < Draper::Decorator
+  delegate_all
+
+  def in_same_minute_with?(other_order)
+    model.created_at.day == other_order.created_at.day and model.created_at.hour == other_order.created_at.hour and model.created_at.minute == other_order.created_at.minute
+  end
+
+end
