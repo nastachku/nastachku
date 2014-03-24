3c3a4b00c179bfd94a5be10df75c75ec8c66c052
Dmitry Davydov <haudvd@gmail.com>
2014-03-24 17:52:10 +0400

    add lectures to join

--- app/controllers/web/schedules_controller.rb
+++ app/controllers/web/schedules_controller.rb
@@ -1,6 +1,7 @@
 class Web::SchedulesController < Web::ApplicationController
   def show
-    @halls = HallDecorator.decorate_collection Hall.includes(:slots).activated.by_position
+    # @halls = HallDecorator.decorate_collection Hall.includes(slots: {lecture: :workshop}).activated.by_position
+    @halls = HallDecorator.decorate_collection Hall.joins(:slots).joins("LEFT OUTER JOIN lectures ON slots.event_id = lectures.id").where('lectures.id = slots.event_id').activated.by_position.uniq
     @slots = Slot.all
     @workshops = Workshop.all
   end
