9dbcf0ef27259ef0fde2f9be0336d02c443597eb
pavel <kalashnikovisme@gmail.com>
2014-03-25 18:44:18 +0400

    remove cyrillic

--- lib/data_helper.rb
+++ lib/data_helper.rb
@@ -2,12 +2,12 @@ module DataHelper
   def restore_data_from_timepad_csv(file_path)
     table =  SmarterCSV.process(file_path)
     table.each do |row|
-      if row[:"'Статус'"] == "'оплачено'"
-        user = User.find_by_email(row[:"'E-mail'"])
+      if row[I18n.t('ru.timepad.data.status').to_sym] == I18n.t('ru.timepad.data.has_paid')
+        user = User.find_by_email(row[I18n.t('ru.timepad.data.email').to_sym])
         if user
           user.pay_part
-          ticket_order = user.ticket_orders.build(cost: row[:"'Сумма'"].to_s.split("'")[1].to_i, items_count: 1)
-          ticket_order.created_at = row[:"'Оплатил'"].to_s.split("'")[1].to_datetime
+          ticket_order = user.ticket_orders.build(cost: row[I18n.t('ru.timepad.data.sum').to_sym].to_s.split("'")[1].to_i, items_count: 1)
+          ticket_order.created_at = row[I18n.t('ru.timepad.data.paid').to_sym].to_s.split("'")[1].to_datetime
           ticket_order.pay
           ticket_order.save
         end
