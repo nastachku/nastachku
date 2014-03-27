bdf3c0346768648ee5173fdb9120fd151a649636
Dmitry Davydov <haudvd@gmail.com>
2014-03-27 17:19:15 +0400

    remove cache from lectures if user is authorized

--- app/helpers/web/user_lectures_helper.rb
+++ app/helpers/web/user_lectures_helper.rb
@@ -1,11 +1,4 @@
 module Web::UserLecturesHelper
-  def user_lectures_cache_key
-    ( proc {
-        lecture = Lecture.voted.with_active_speaker.order('updated_at DESC').limit(1).first
-        {:tag => "#{lecture.updated_at.to_i if lecture}_#{params.except(:_).to_s}_#{current_user.id}"}
-    } ).call
-  end
-  
   def user_voted_for_lecture?(lecture)
     lecture.lecture_votings.voted_by? current_user
   end
