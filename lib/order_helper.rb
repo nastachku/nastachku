a6b0831c462326c9c741208190e3b7e83ff1341a
pavel <kalashnikovisme@gmail.com>
2014-03-27 13:09:06 +0400

    fix order_helper

--- lib/order_helper.rb
+++ lib/order_helper.rb
@@ -1,4 +1,9 @@
 module OrderHelper
+  def pay_part_if_ticket_order(order)
+    if other_order.type = "TicketOrder"
+      other_order.user.pay_part
+    end
+  end
   def put_paid_status_with_other_orders(order)
     order_cost = order.cost
     order.user.orders.each do |other_order|
@@ -8,10 +13,12 @@ module OrderHelper
       if order.decorate.in_same_minute_with?(other_order)
         if order_cost == other_order.its_cost
           other_order.pay
+          pay_part_if_ticket_order other_order
           break;
         elsif order_cost > other_order.its_cost
           if (other_order.its_cost == configus.platidoma.ticket_price and order_cost - configus.platidoma.ticket_price == configus.platidoma.afterparty_price) or (other_order.its_cost == configus.platidoma.afterparty_price and order_cost - configus.platidoma.afterparty_price == configus.platidoma.ticket_price)
             other_order.pay
+            pay_part_if_ticket_order other_order
             order_cost -= other_order.its_cost
           end
         end
