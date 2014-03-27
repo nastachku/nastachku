7001edacccdf378b65b0f976d04b78f0ff7b8c42
Dmitry Davydov <haudvd@gmail.com>
2014-03-27 18:33:16 +0400

    add cache to schedule

--- app/controllers/web/schedules_controller.rb
+++ app/controllers/web/schedules_controller.rb
@@ -1,4 +1,9 @@
 class Web::SchedulesController < Web::ApplicationController
+  caches_action :show, :cache_path => Proc.new {|c|
+    hall = Hall.activated.order('updated_at DESC').limit(1).first
+    {:tag => "#{hall.updated_at.to_i if hall}"}
+  }
+  
   def show
     @halls = HallDecorator.decorate_collection Hall.includes(:slots).activated.by_position
     @slots = Slot.all
--- app/decorators/lecture_decorator.rb
+++ app/decorators/lecture_decorator.rb
@@ -12,9 +12,9 @@ class LectureDecorator < Draper::Decorator
 
   def workshops_color_hash
     @workshops_hash ||= Workshop.all
-    hash = { "#{workshops_hash[0].title}" => "green", "#{workshops_hash[1].title}" => "yellow",
-      "#{workshops_hash[2].title}" => "orange", "#{workshops_hash[3].title}" => "blue",
-      "#{workshops_hash[4].title}" => "red", "#{workshops_hash[5].title}" => "purple"}
+    hash = { "#{@workshops_hash[0].title}" => "green", "#{@workshops_hash[1].title}" => "yellow",
+      "#{@workshops_hash[2].title}" => "orange", "#{@workshops_hash[3].title}" => "blue",
+      "#{@workshops_hash[4].title}" => "red", "#{@workshops_hash[5].title}" => "purple"}
     hash
   end
 
