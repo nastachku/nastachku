7df19a5bd6b23dedbb6c9eeb6e357ee47957a06b
Dmitry Davydov <haudvd@gmail.com>
2014-04-01 01:17:37 +0400

    fix encoding

--- lib/human_name_validator.rb
+++ lib/human_name_validator.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class HumanNameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /^[a-zA-Zа-яА-ЯёЁ][a-zA-Zа-яА-ЯёЁ '`-]{0,253}[a-zA-Zа-яА-ЯёЁ]{0,1}$/
