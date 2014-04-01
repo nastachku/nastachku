8066160699a2b52326eea1044b63135160954e3b
pavel <kalashnikovisme@gmail.com>
2014-04-01 15:09:48 +0400

    refactor shop client

--- lib/cs_cart_helper.rb
+++ lib/cs_cart_helper.rb
@@ -1,12 +1,9 @@
-module CsCartHelper
+require "cs_cart/client"
 
+module CsCartHelper
+  include CsCart::Client
   def auth_cs_cart_user_url(token)
     "https://devel.cs-cart.ru/index.php?dispatch=auth.login_stachka&token=#{token}"
-  end
-
-  def get_token(args)
-    response = client_proxy.get_token(args).perform
-    ActiveSupport::JSON.decode(response.body)["data"] if response.success?
   end
 
   def get_auth_token(user)
@@ -19,19 +16,4 @@ module CsCartHelper
     }
     token = get_token args
   end
-
-  def auth_user(args)
-    response = @client_proxy.auth_user args
-  end
-
-  def client_proxy
-    @client_proxy ||= ClientProxy.new
-  end
-
-  class ClientProxy < Weary::Client
-    post :get_token, "https://devel.cs-cart.ru/index.php?dispatch=auth.get_stachka_token" do |resource|
-      resource.required :first_name, :last_name, :stachka_id, :email, :key
-    end
-  end
-
 end
