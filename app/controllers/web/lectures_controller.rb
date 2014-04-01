3c4e63ea901e9bd8ea50353d6cceea2e4738400d
Dmitry Davydov <haudvd@gmail.com>
2014-04-01 14:00:41 +0400

    update querys

--- app/controllers/web/lectures_controller.rb
+++ app/controllers/web/lectures_controller.rb
@@ -5,10 +5,10 @@ class Web::LecturesController < Web::ApplicationController
   }
 
   def index
-    @lectures = LectureDecorator.decorate_collection Lecture.web.with_active_speaker.by_listener_votes.ransack(params).result
+    @lectures = LectureDecorator.decorate_collection Lecture.includes(:workshop, :user).web.with_active_speaker.by_listener_votes.ransack(params).result
+    @current_user_votings = LectureVoting.where(user_id: current_user.id) if current_user
     @lecture_id = params[:lecture_id]
     @lecture_by_id = LectureDecorator.decorate Lecture.find @lecture_id if @lecture_id
-    @workshops = Workshop.web
     respond_to do |format|
        format.html
        format.js
