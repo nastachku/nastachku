60cdc42d5a6778d979b7bad888f59021a64ac03e
Dmitry Davydov <haudvd@gmail.com>
2014-04-02 23:52:06 +0400

    fix schedule hall

--- config/initializers/load_models.rb
+++ config/initializers/load_models.rb
@@ -1,14 +1,19 @@
-if Rails.env == "development"
-  def require_all_in(folder)
-    Dir.foreach(folder) do |name|
-      if name =~ /\.rb$/
-        puts "REQUIRING DEPENDENCY: #{name}"
-        require_dependency name unless name == "." || name == ".."
-      else
-        puts "SKIP #{name}"
+def load_models_if_development_initilizer
+  if Rails.env == "development"
+    def require_all_in(folder)
+      Dir.foreach(folder) do |name|
+        if name =~ /\.rb$/
+          puts "REQUIRING DEPENDENCY: #{name}"
+          require_dependency name unless name == "." || name == ".."
+        else
+          puts "SKIP #{name}"
+        end
       end
     end
+    require_all_in("#{Rails.root}/app/models")
+    require_all_in("#{Rails.root}/app/decorators")
   end
-  require_all_in("#{Rails.root}/app/models")
-  require_all_in("#{Rails.root}/app/decorators")
+end
+ActionDispatch::Reloader.to_prepare do
+  load_models_if_development_initilizer
 end
