0c2f8e71b1744e7123fdfd1f6d64482034ae04df
Dmitry Davydov <haudvd@gmail.com>
2014-04-03 20:50:45 +0400

    fix job

--- app/jobs/BroadcastMailerJob.rb
+++ app/jobs/BroadcastMailerJob.rb
@@ -5,7 +5,7 @@ class BroadcastMailerJob
     users = User.all
     users.each_with_index do |user, i|
       UserMailer.broadcast(user.id, mail_params_id).deliver_in((10 * (i + 1)).seconds)
-      logger.info "BROADCASTING SEND EMAILS #{user.id}"
+      Rails.logger.info "BROADCASTING SEND EMAILS #{user.id}"
       sleep 1.second
     end
   end
