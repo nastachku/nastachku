435364cdc2397fa8c9051ace805632a5ac91e24f
pavel <kalashnikovisme@gmail.com>
2014-04-08 23:09:10 +0400

    fix

--- app/jobs/BroadcatMailerJobAfterCreate.rb
+++ app/jobs/BroadcatMailerJobAfterCreate.rb
@@ -3,8 +3,8 @@ class BroadcastMailerJobAfterCreate
 
   def self.perform(users)
     users.each_with_index do |user, i|
-      UserMailer.sent_after_create_if_user_present(user.id).deliver_in((10 * (i + 1)).seconds)
-      Rails.logger.info "BROADCASTING SEND EMAILS #{user.id}"
+      UserMailer.sent_after_create_if_user_present(user[:id]).deliver_in((10 * (i + 1)).seconds)
+      Rails.logger.info "BROADCASTING SEND EMAILS #{user[:id]}"
       sleep 1.second
     end
   end
