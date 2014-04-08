c58eea57402a70283117aace9fbf7b8464201b9a
pavel <kalashnikovisme@gmail.com>
2014-04-08 14:17:20 +0400

    replace timepad data helper

---
+++ lib/data/users_lists_data_helper.rb
@@ -0,0 +1,13 @@
+module Data::UsersListsDataHelper
+  def upload_list_from_file
+    table = SmarterCSV.process(file_path)
+    users_list = []
+    table.map do |row|
+      user = User.find_by_email row[I18n.t('users_lists.data.status').to_sym]
+      if user
+        users_lists << user
+      end
+    end
+    users_lists
+  end
+end
