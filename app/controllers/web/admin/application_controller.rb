062f66efe09e92751fe7f2652d2034b87b4d7713
pavel <kalashnikovisme@gmail.com>
2014-04-08 21:39:08 +0400

    fix uploading lists

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,5 +1,4 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
-  include DataHelper
   before_filter :authenticate_admin!
   include Data::UsersListsDataHelper
   include Data::TimepadDataHelper
