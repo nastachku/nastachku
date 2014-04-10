eca494f84abe753857de9c1fdf09a5fa1646163d
pavel <kalashnikovisme@gmail.com>
2014-04-10 18:26:39 +0400

    fix days and button

--- app/assets/javascripts/web/schedules.js
+++ app/assets/javascripts/web/schedules.js
@@ -21,3 +21,15 @@ function check_my_program_slots(slots) {
     }
   });
 }
+$(document).ready(function() {
+  function digitalWatch() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    $(".programm__timer-time").html(hours + "<span>" + minutes + "</span>");
+    setTimeout(function() { digitalWatch(); } , 60000);
+  }
+  digitalWatch();
+});
