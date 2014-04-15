efb08c55a6db8258228e5348747b4c7ca87b757d
pavel <kalashnikovisme@gmail.com>
2014-04-16 03:50:59 +0400

    add lectures sorted by time test

--- test/decorators/hall_decorator_test.rb
+++ test/decorators/hall_decorator_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class HallDecoratorTest < Draper::TestCase
+  setup do
+    @hall = (create :hall).decorate
+  end
+
+  test "slots sorted by name" do
+    @hall.slots << (create :slot)
+    assert @hall.lectures_sorted_by_time.any?
+  end
 end
