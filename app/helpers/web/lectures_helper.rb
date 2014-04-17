fddcdace6cb6a411c0d686adff7dec19c5dc4b21
pavel <kalashnikovisme@gmail.com>
2014-04-17 15:57:40 +0400

    add test for lectures helper

--- app/helpers/web/lectures_helper.rb
+++ app/helpers/web/lectures_helper.rb
@@ -1,9 +1,5 @@
 module Web::LecturesHelper
-  def user_is_listener?(lecture)
-    lecture.lecture_votings.voted_by? current_user
-  end
-
-  def lecture_voted_by_current_user(lecture, current_user_votings)
+  def lecture_voted_by_current_user?(lecture, current_user_votings)
     current_user_votings.select {|voting| voting.voteable_id == lecture.id}.any?
   end
 end
