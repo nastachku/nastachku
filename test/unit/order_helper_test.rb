f74502442b8ab0447f3de7116a81142729de90b0
pavel <kalashnikovisme@gmail.com>
2014-04-17 14:06:20 +0400

    add tests for order helper

--- test/unit/order_helper_test.rb
+++ test/unit/order_helper_test.rb
@@ -10,4 +10,30 @@ class OrderHelperTest < ActiveSupport::TestCase
     pay_user_if_ticket order
     assert order.user.paid_part?
   end
+
+  test "ticket order one of two items" do
+    order = create :ticket_order
+    order.cost = configus.platidoma.ticket_price
+    order.save
+    assert ticket_order_one_of_two_items? order, configus.platidoma.afterparty_price + configus.platidoma.ticket_price
+  end
+
+  test "afterparty order one of two items" do
+    order = create :afterparty_order
+    order.cost = configus.platidoma.afterparty_price
+    order.save
+    assert afterparty_one_of_two_items? order, configus.platidoma.afterparty_price + configus.platidoma.ticket_price
+  end
+
+  test "put paid status with other orders" do
+    user = create :user
+    order = create :order
+    order.cost = configus.platidoma.ticket_price
+    order.save
+    order.pay
+    user.orders << order
+    user.orders << (create :ticket_order)
+    put_paid_status_with_other_orders order
+    assert user.orders.last.paid?
+  end
 end
