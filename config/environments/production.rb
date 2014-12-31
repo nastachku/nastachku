2d0a7da1da773dd058aeebee947a7cb3afb2131c
Alex Klyanchin <jetblackmeerkat@gmail.com>
2014-12-31 16:47:52 +0300

    fix application configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,17 @@
 Nastachku::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = true
-  config.assets.compress = true
+  config.serve_static_files = true # FIXME: этой фигней в продакшне должен заниматься веб-сервер
+  config.assets.js_compressor = :uglifier
   config.assets.compile = true
   config.assets.digest = true
   config.assets.precompile += %w( ckeditor/* )
   config.assets.precompile += %w( web/application.js web/application.css admin/application.js admin/application.css *.ttf *.svg *.eot *.woff )
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+
   GA.tracker = "UA-38587983-1"
   ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,22 +7,24 @@ Nastachku::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  config.eager_load = true
+
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
+  config.action_controller.allow_forgery_protection = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
@@ -34,4 +36,6 @@ Nastachku::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :random
 end
