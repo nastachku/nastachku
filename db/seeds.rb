ba9c935e818c4d6a723e83dda2e78ac666f733ef
Anton Vasin <antonvasinv@gmail.com>
2015-01-23 14:12:58 +0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,15 @@
 
 user = User.find_or_initialize_by(email: configus.admin.email) do |u|
   u.password = configus.admin.password
-  u.first_name = configus.admin.first_name 
-  u.last_name = configus.admin.last_name 
+  u.first_name = configus.admin.first_name
+  u.last_name = configus.admin.last_name
   u.city = configus.admin.city
   u.admin = true
+  u.company = "Компания"
   u.save!
   u.activate
   u.attend
   u.pay_part
 end
-User::PromoCode.create({ code: Array.new(9){ rand(36).to_s(36) }.join, user_id: user.id })
+
+User::PromoCode.create code: Array.new(9){ rand(36).to_s(36) }.join, user_id: user.id
