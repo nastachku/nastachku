b76cb4290794a4b574abb31a9e8065e011a974b2
Anton Vasin <antonvasinv@gmail.com>
2015-02-04 19:21:20 +0300

    Single unicorn config file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # coding: UTF-8
 
-APP_PATH = "/u/apps/nastachku_2015"
+APP_PATH = "/u/apps/nastachku"
 
 # Sample verbose configuration file for Unicorn (not Rack)
 #
@@ -14,7 +14,7 @@ APP_PATH = "/u/apps/nastachku_2015"
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 1
+worker_processes 2
 
 # Since Unicorn is never exposed to outside clients, it does not need to
 # run on the standard HTTP port (80), there is no reason to start Unicorn
