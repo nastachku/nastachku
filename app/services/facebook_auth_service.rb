c6814efc2446c1c3f35a0167203c3898bba8ff5f
Jana Petrova <janavpetrova@gmail.com>
2015-02-08 18:44:10 +0000

    fix tests

--- app/services/facebook_auth_service.rb
+++ app/services/facebook_auth_service.rb
@@ -12,9 +12,6 @@ class FacebookAuthService
         user.authorizations << authorization
       end
 
-      # FIXME странная концепция с промо-кодами. Выпилить это, когда приведутся в порядок промо-коды
-      User::PromoCode.create({ code: generate_promo_code, user_id: user.id }) unless user.promo_code
-
       user
     end
 
