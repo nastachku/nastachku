33f8d7e07bed2ddfaaa23314332e8cce32dfce4f
Jana Petrova <janavpetrova@gmail.com>
2015-02-10 14:37:22 +0000

    fix registration issues #001, #002, #004

--- app/types/user_registration_type.rb
+++ app/types/user_registration_type.rb
@@ -3,7 +3,7 @@ class UserRegistrationType < User
 
   has_secure_password
 
-  attr_accessible :process_personal_data
+  attr_accessible :process_personal_data, :password_confirmation
 
   permit :password_confirmation, :password, :state_event, :process_personal_data,
          :first_name, :last_name, :city, :company, :position, :email
