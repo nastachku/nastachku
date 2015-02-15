7a0cd42515b138513e77db56108b81d970943293
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-02-15 23:12:27 +0300

    remove useless js

--- app/assets/javascripts/web/accounts.js
+++ app/assets/javascripts/web/accounts.js
@@ -14,16 +14,4 @@ jQuery(document).ready(function ($) {
     $(this).siblings().removeClass('selected').end().next('dd').andSelf().addClass('selected');
   });
 
-  $('#i_will_come_to_stachka').change(function() {
-    if (this.checked) {
-      $.ajax({
-        url: $(this).data('url'),
-        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-        type: "PUT"
-      });
-    } else {
-      this.checked = true;
-    }
-  });
-
 });
--- app/assets/javascripts/web/users.js
+++ app/assets/javascripts/web/users.js
@@ -8,11 +8,9 @@ jQuery(document).ready(function ($) {
     $.getScript($('#filter_for_users option:selected').data("href"));
     return false;
   });
-})
 
-$(document).ready(function (e) {
   $("#submit").attr("disabled", true);
   $("#privacy").click(function () {
     $("#submit").attr("disabled", !this.checked);
   });
-});
+})
