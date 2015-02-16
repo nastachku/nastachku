c20a9749e68baefe90d36fc81a7e06417aee5de9
Anton Vasin <antonvasinv@gmail.com>
2015-02-16 19:07:34 +0300

    Fix style

--- app/types/user_lectures_edit_type.rb
+++ app/types/user_lectures_edit_type.rb
@@ -1,10 +1,8 @@
 class UserLecturesEditType < User
   include BasicType
 
-  accepts_nested_attributes_for :lectures #, reject_if: :all_blank
-
-  permit :photo, :about,
-    lectures_attributes: [:id, :_destroy, :title, :thesises, :workshop_id, :presentation, :user_id]
+  permit :photo, :about, lectures_attributes: [
+    :id, :_destroy, :title, :thesises, :workshop_id, :presentation, :user_id]
 
   validates :photo, presence: true
   validates :about, presence: true
--- test/functional/web/account/lectures_controller_test.rb
+++ test/functional/web/account/lectures_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Web::Account::LecturesControllerTest < ActionController::TestCase
   setup do
@@ -7,12 +7,12 @@ class Web::Account::LecturesControllerTest < ActionController::TestCase
     @user_attrs = generate(:user_with_lectures)
   end
 
-  test '#new' do
+  test "#new" do
     get :new
     assert_response :success
   end
 
-  test '#create' do
+  test "#create" do
     post :create, user: @user_attrs
     assert_response :redirect
   end
