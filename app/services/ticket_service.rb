6be6729bb5e0a09bb084a80cae894e7765806866
Jana Petrova <janavpetrova@gmail.com>
2015-02-20 15:58:24 +0000

    fix ticket system bugs

--- app/services/ticket_service.rb
+++ app/services/ticket_service.rb
@@ -5,7 +5,8 @@ class TicketService
 
       order = user.ticket_orders
                   .create(cost: ticket_code.price,
-                          payment_system: 'paper_ticket')
+                          payment_system: 'paper_ticket',
+                          items_count: 1)
       order.transaction_id = ticket_code.code
 
       order.pay!
