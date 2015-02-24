d82d02c48f7445d22d3b7d3317b8eb344ce4dcae
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-02-24 05:10:24 +0300

    remove empty tests

--- test/unit/hall_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HallTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/user/promo_code_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class User::PromoCodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
