59d9074392831904daae8b5f5dbb5fbf4e3eea2f
Anton Vasin <antonvasinv@gmail.com>
2015-02-26 19:29:11 +0300

    Find or create ticket by code

--- app/services/activate_ticket.rb
+++ app/services/activate_ticket.rb
@@ -2,8 +2,7 @@ class ActivateTicket
   def self.call(user, ticket_code)
     ActiveRecord::Base.transaction do
       user.create_ticket! price: ticket_code.price
-
-      user.ticket.ticket_code = ticket_code
+      user.ticket = Ticket.create_with(price: ticket_code.price).find_or_create_by!(ticket_code: ticket_code)
       ticket_code.activate!
     end
   end
