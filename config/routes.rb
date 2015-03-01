90817851a94db5f02333f4f26959bc865569ace1
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-03-01 20:59:23 +0300

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -129,7 +129,7 @@ Nastachku::Application.routes.draw do
       resources :lectures
       resources :pages
       resources :news
-      resources :users
+      resources :users, except: [:new, :create]
       resources :audits, only: [:index]
       resources :topics
       resources :distributors, except: :show
