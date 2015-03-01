353f53f1b998d0bd7a6531d46406e41f524f9b82
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-03-01 21:32:15 +0300

    set email to nil if blank

--- app/types/admin/user_edit_type.rb
+++ app/types/admin/user_edit_type.rb
@@ -6,4 +6,8 @@ class Admin::UserEditType < User
   permit :state_event, :pay_state_event, :first_name, :last_name, :city, :company, :about, :in_carousel,
     :invisible_lector, :carousel_info, :position, :email, :photo, :show_as_participant, :admin, :role,
     :badge_state_event, :reason_to_give_ticket
+
+  def email=(val)
+    self[:email] = nil if val.blank?
+  end
 end
