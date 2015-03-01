38f703e26ba68d835f7732195ec004dcf1561b76
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-03-01 20:14:53 +0300

    add wrong
    remove ruby-prof, guard
    reorder gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,10 +10,12 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/setup'
+require 'wrong'
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 class ActiveSupport::TestCase
+  include Wrong
   include AuthHelper
   include SocNetworkHelper
   require 'factory_girl_rails'
