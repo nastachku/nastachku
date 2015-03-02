69f36e4c6cb446da82bbf280eb1af145348cf5cf
Anton Vasin <antonvasinv@gmail.com>
2015-03-02 17:03:13 +0300

    Add test to web/welcome

---
+++ test/functional/web/welcome_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class Web::WelcomeControllerTest < ActionController::TestCase
+  test '#index' do
+    get :index
+    assert_response :success
+  end
+
+  test '#landing' do
+    get :index
+    assert_response :success
+  end
+end
