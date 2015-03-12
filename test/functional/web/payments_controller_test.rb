9d4970ecacca5a66f69f5aa056aa29b2afe966e8
Alexander Shcherbinin <realmyst@ya.ru>
2015-03-13 02:23:44 +0300

    fix test

--- test/functional/web/payments_controller_test.rb
+++ test/functional/web/payments_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class Web::PaymentsControllerTest < ActionController::TestCase
   test 'redirects to buy now' do
-    order = create :order, :with_tickets, user: nil
+    order = create :order, :with_tickets, user: nil, from: :buy_now
     get :success_payanyway, MNT_TRANSACTION_ID: order.number
     assert_redirected_to success_buy_now_path(order_number: order.number)
   end
 
   test 'redirects to account' do
-    order = create :order, :with_tickets
+    order = create :order, :with_tickets, from: :account_order
     get :success_payanyway, MNT_TRANSACTION_ID: order.number
     assert_redirected_to edit_account_path(anchor: :orders)
   end
