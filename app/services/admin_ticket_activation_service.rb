6fff23b5755d12558c24f81f7ac2513da640c8a8
Alexander Shcherbinin <realmyst@ya.ru>
2015-04-07 03:37:48 +0300

    add ticket code validation

--- app/services/admin_ticket_activation_service.rb
+++ app/services/admin_ticket_activation_service.rb
@@ -5,22 +5,24 @@ class AdminTicketActivationService
 
   def activate_code(code)
     ticket_code = TicketCode.can_activate.find_by(code: code)
-    case ticket_code.kind
-    when "conference"
-      if @user.ticket.present?
-        @user.ticket.cancel
+    @user.transaction do
+      case ticket_code.kind
+      when "conference"
+        if @user.ticket.present?
+          @user.ticket.cancel
+        end
+        ticket = @user.build_ticket
+      when "party"
+        if @user.afterparty_ticket.present?
+          @user.afterparty_ticket.cancel
+        end
+        ticket = @user.build_afterparty_ticket
       end
-      ticket = @user.build_ticket
-    when "party"
-      if @user.afterparty_ticket.present?
-        @user.afterparty_ticket.cancel
-      end
-      ticket = @user.build_afterparty_ticket
-    end
 
-    ticket_code.activate
-    ticket.ticket_code = ticket_code
-    ticket.price = ticket_code.price
-    ticket.save
+      ticket_code.activate
+      ticket.ticket_code = ticket_code
+      ticket.price = ticket_code.price
+      ticket.save
+    end
   end
 end
--- app/types/admin/user_ticket_code_activation_type.rb
+++ app/types/admin/user_ticket_code_activation_type.rb
@@ -4,4 +4,16 @@ class Admin::UserTicketCodeActivationType
   attribute :code, String
 
   validates :code, presence: true
+  validate :existing_code, if: :code
+
+  def existing_code
+    ticket_code = TicketCode.find_by(code: code)
+    if ticket_code
+      unless ticket_code.new?
+        errors.add(:code, :cant_activate)
+      end
+    else
+      errors.add(:code, :code_not_exist)
+    end
+  end
 end
