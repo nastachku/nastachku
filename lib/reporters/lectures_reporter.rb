3fdcc7122cedef42a6c1b0adfca730709469d91b
Emperor <valentine.emperor@gmail.com>
2016-03-22 22:46:25 +0300

    fixed for userless lectures

--- lib/reporters/lectures_reporter.rb
+++ lib/reporters/lectures_reporter.rb
@@ -4,9 +4,9 @@ class Reporters::LecturesReporter < Reporters::Base
       lecture_state = lecture.slot.present? ? 'В программе' : lecture.human_state_name
 
       [
-        lecture.title, lecture_state, lecture.user.full_name,
-        lecture.user.city, lecture.user.company, lecture.user.position,
-        lecture.user.email, lecture.user.phone, lecture.user.skype
+        lecture.title, lecture_state, lecture.user.try(:full_name),
+        lecture.user.try(:city), lecture.user.try(:company), lecture.user.try(:position),
+        lecture.user.try(:email), lecture.user.try(:phone), lecture.user.try(:skype)
       ]
     end
 
