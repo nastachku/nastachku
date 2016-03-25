479fed6ee9a93a320584a8f1a4c770ea60b54304
Emperor <valentine.emperor@gmail.com>
2016-03-25 13:16:05 +0300

    top lectures for user_lectures

--- app/controllers/web/user_lectures_controller.rb
+++ app/controllers/web/user_lectures_controller.rb
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 class Web::UserLecturesController < Web::ApplicationController
   def index
-    @lectures_with_lector = LectureDecorator.decorate_collection Lecture.includes(:workshop, :user).voted.with_active_speaker.by_created_at.ransack(params[:q]).result
-    @lectures_without_lector = LectureDecorator.decorate_collection Lecture.includes(:workshop).voted.without_speaker.by_created_at.ransack(params[:q]).result
-    @lectures = @lectures_with_lector + @lectures_without_lector
+    @top_lectures = LectureDecorator.decorate_collection Lecture.includes(:workshop).voted.by_created_at.by_created_at.ransack(params[:q]).result
+    @lectures_with_lector = LectureDecorator.decorate_collection Lecture.includes(:workshop, :user).voted.with_active_speaker.where.not(id: @top_lectures.map(&:id)).by_created_at.ransack(params[:q]).result
+    @lectures_without_lector = LectureDecorator.decorate_collection Lecture.includes(:workshop).voted.without_speaker.where.not(id: @top_lectures.map(&:id)).by_created_at.ransack(params[:q]).result
+    @lectures = @top_lectures + @lectures_with_lector + @lectures_without_lector
     @current_user_votings = LectureVoting.where(user_id: current_user.id) if current_user
     # вынести в show, если возможно, не забыть про хак в application.rb
     @lecture_id = params[:lecture_id]
